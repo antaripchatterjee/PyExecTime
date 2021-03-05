747065b94ac93b2442102df62ec3c35b4ece1322
Antarip Chatterjee <antarip.chatterjee22@gmail.com>
2021-03-05 22:34:37 +0530

    Initial Commit. Context Manager class PyExecTime and decorator function py_exec_time has been added.

---
+++ test.py
@@ -0,0 +1,5 @@
+from pyexectime.pyexectime_test import test_clm, test_dec
+
+
+test_clm() # PyExecTime [(test_pkg\pyexectime_test.py) 4:6] -> Execution took 0.000417 seconds
+test_dec(10000) # PyExecTime [(test.py) 5:5] -> Execution took 0.000405 seconds
